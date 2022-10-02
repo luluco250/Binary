6310a93d3c3cab7d019b422d1fbc102fc118c997
Lucas Melo <luluco250@gmail.com>
2022-10-02 14:44:28 -0700

    Creating fork.
    - Bump to .NET Framework 4.8.
    - Convert to SDK-style csproj.
    - Remove admin and password checks.

--- Binary/Program.cs
+++ Binary/Program.cs
@@ -22,64 +22,23 @@
  * SOFTWARE. 
 ********************************************************************************/
 
-
-
-using System;
-using System.Threading;
 using System.Globalization;
-using System.Windows.Forms;
-using System.Security.Principal;
 
+namespace Binary;
 
-
-namespace Binary
+static class Program
 {
-	static class Program
+	[STAThread]
+    static void Main()
 	{
-		/// <summary>
-		/// The main entry point for the application.
-		/// </summary>
-		[STAThread]
-        [Obsolete]
-        static void Main()
-		{
-            // Skip administator check if in the debug mode
-            if (System.Diagnostics.Debugger.IsAttached)
-                goto LABEL_DEBUG_SKIP;
+        Application.EnableVisualStyles();
+        Application.SetCompatibleTextRenderingDefault(false);
 
-            // Check if the program is run as administator, exit if not
-            using (var identity = WindowsIdentity.GetCurrent())
-            {
-                var principal = new WindowsPrincipal(identity);
-                if (!principal.IsInRole(WindowsBuiltInRole.Administrator))
-                {
-                    MessageBox.Show("Run Binary in Administrator mode!", "Warning",
-                        MessageBoxButtons.OK, MessageBoxIcon.Warning);
-                    return;
-                }
-            }
+        var culture = CultureInfo.CreateSpecificCulture("en-US");
+        Thread.CurrentThread.CurrentCulture = culture;
+        CultureInfo.DefaultThreadCurrentCulture = culture;
+        CultureInfo.DefaultThreadCurrentUICulture = culture;
 
-        LABEL_DEBUG_SKIP:
-            AppDomain.CurrentDomain.AppendPrivatePath("Libraries");
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-
-            var culture = CultureInfo.CreateSpecificCulture("en-US");
-            Thread.CurrentThread.CurrentCulture = culture;
-            CultureInfo.DefaultThreadCurrentCulture = culture;
-            CultureInfo.DefaultThreadCurrentUICulture = culture;
-
-            // If password check was not done yet
-            if (!Properties.Settings.Default.PasswordPassed)
-            {
-                Application.Run(new Interact.Welcome());
-            }
-
-            // Run if was done/entered
-            if (Properties.Settings.Default.PasswordPassed)
-            {
-                Application.Run(new Main.Main());
-            }
-        }
+        Application.Run(new Main.Main());
     }
 }
